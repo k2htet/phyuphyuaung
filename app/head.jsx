4c007cf40252b747ff33245528202c7401e0a457
KoHtet <kokoh6329@gmail.com>
2023-02-01 20:48:39 +0630

    add meta name

--- app/head.jsx
+++ app/head.jsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Ko Ko Htet</title>
+      <meta name="title" content="Ko Ko Htet - Frontend Web Developer"></meta>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="frontend web developer" />
     </>
