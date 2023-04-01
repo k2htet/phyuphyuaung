73bc50045f52e2c5bc0433cc5cd163a5faf7fb32
KoHtet <kokoh6329@gmail.com>
2023-04-01 11:16:39 +0630

    for ppa

--- app/head.jsx
+++ app/head.jsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>Ko Ko Htet</title>
+      <title>Phyu Phyu Aung</title>
       <meta name="title" content="Ko Ko Htet - Frontend Web Developer"></meta>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="frontend web developer" />
+      <meta name="description" content="laravel web developer" />
     </>
   );
 }
